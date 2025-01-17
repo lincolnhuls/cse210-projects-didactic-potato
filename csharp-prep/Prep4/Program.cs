ec15cdd8eea4be3cea9743627524f9dafa7fd080
lincolnhuls <hul21001@byui.edu>
2025-01-17 13:27:59 -0700

    Added the assignments for prep 4 and 5

--- csharp-prep/Prep4/Program.cs
+++ csharp-prep/Prep4/Program.cs
@@ -1,9 +1,51 @@
 using System;
+using System.Globalization;
+using System.Collections.Generic;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep4 World!");
+        Console.WriteLine("Enter a list of numbers, type 0 when finished:  ");
+        List<int> numbers = new List<int>();
+        int user_number = 0;
+        int smallest_so_far = 1000000;
+        int largest_so_far = 0;
+        int total = 0;
+        do 
+        {
+            Console.Write("Enter number: ");
+            string string_number = Console.ReadLine();
+            user_number = int.Parse(string_number);
+            numbers.Add(user_number);
+        } while (user_number != 0);
+        foreach (int number in numbers)
+        {
+            total += number;
+            if (number > 0)
+            {
+                if (number > largest_so_far)
+                {
+                    largest_so_far = number;
+                }
+                if (number < smallest_so_far)
+                {
+                    smallest_so_far = number;
+                }
+            }
+        }
+        numbers.Sort();
+        int average_divide = (numbers.Count) - 1;
+        float average = (float)total / (float)(average_divide);
+        numbers.Remove(0);
+        Console.WriteLine($"The total is {total}.");
+        Console.WriteLine($"The average is {average}.");
+        Console.WriteLine($"The biggest number is {largest_so_far}");
+        Console.WriteLine($"The smallest positive number is {smallest_so_far}.");
+        Console.WriteLine("Sorted list:");
+        foreach (int number in numbers)
+        {
+            Console.WriteLine(number);
+        }
     }
 }
--- csharp-prep/Prep5/Program.cs
+++ csharp-prep/Prep5/Program.cs
@@ -1,9 +1,44 @@
 using System;
+using System.Reflection.Metadata;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep5 World!");
+        static void DisplayMessage()
+        {
+            Console.WriteLine("Welcome to the program!");
+        }
+        static string PromptUserName()
+        {
+            Console.Write("Please enter your name: ");
+            string name = Console.ReadLine();
+            return name;
+        } 
+        static int PromptUserNumber()
+        {
+            Console.Write("Please enter your favorite number: ");
+            string user_number = Console.ReadLine();
+            int number = int.Parse(user_number);
+            return number;
+        }
+        static int SquareNumber(int number)
+        {
+            int squared_number = number * number;
+            return squared_number;
+        }
+        static void DisplayResults(string name, int number) 
+        {
+            Console.WriteLine($"{name}, the square of your favorite number is {number}.");
+        }
+        static void Main()
+        {
+            DisplayMessage();
+            string name = PromptUserName();
+            int number = PromptUserNumber();
+            int squared_number = SquareNumber(number);
+            DisplayResults(name, squared_number);
+        }
+        Main();
     }
 }
