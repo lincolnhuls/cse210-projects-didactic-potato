0847b83b28fd00e8e19a4cd456dbc2b38d770ded
lincolnhuls <hul21001@byui.edu>
2025-03-04 14:40:14 -0700

    Finished the learning 5 assignment

--- prepare/Learning05/Program.cs
+++ prepare/Learning05/Program.cs
@@ -4,6 +4,16 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning05 World!");
+        List<Shape> shapes = new List<Shape>();
+        shapes.Add(new Circle("Red", 2.0));
+        shapes.Add(new Rectangle("Blue", 2.0, 3.0));
+        shapes.Add(new Square("Green", 2.0));
+        shapes.Add(new Circle("Yellow", 3.0));
+        shapes.Add(new Rectangle("Black", 3.0, 4.0));
+        shapes.Add(new Square("White", 3.0));
+        foreach (Shape shape in shapes)
+        {
+            Console.WriteLine($"Color: {shape.GetColor()}, Area: {shape.GetArea()}");
+        }
     }
 }
